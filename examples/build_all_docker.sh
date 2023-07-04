ad2493d1f8403800cdbbd9b5bae75191e740d30c
Tomasz Kurcz <uint@users.noreply.github.com>
2023-07-04 21:15:46 +0200

    build scripts: enable using a persistent cache

--- examples/build_all_docker.sh
+++ examples/build_all_docker.sh
@@ -6,17 +6,34 @@ CHECK=0
 pushd $(dirname ${BASH_SOURCE[0]})
 
 # Loop through arguments and process them
-for arg in "$@"; do
-    case $arg in
-    -c | --check)
-        CHECK=1
-        shift
-        ;;
+VALID_ARGS=$(getopt -o c --long check,cargo-cache-dir: -- "$@")
+if [[ $? -ne 0 ]]; then
+    exit 1
+fi
+
+eval set -- "$VALID_ARGS"
+args=()
+while [ : ]; do
+    case "$1" in
+        -c | --check)
+            CHECK=1
+            shift
+            ;;
+        --cargo-cache-dir)
+            args+=(--cargo-cache-dir "$2")
+            shift 2
+            ;;
+        --)
+            shift
+            break
+            ;;
     esac
 done
 
+echo "args: $args"
+
 for d in "status-message/" $(ls -d */ | grep -v -e "status-message\/$"); do
-    (./build_docker.sh ${d%%/})
+    (./build_docker.sh ${d%%/} "${args[@]}")
 done
 
 if [ $CHECK == 1 ] && [ ! -z "$(git diff --exit-code)" ]; then
--- examples/build_docker.sh
+++ examples/build_docker.sh
@@ -9,6 +9,24 @@ NC='\033[0m'
 
 NAME="$1"
 USER_ID="$(id -u)"
+VALID_ARGS=$(getopt --long cargo-cache-dir: -- "$@")
+if [[ $? -ne 0 ]]; then
+    exit 1
+fi
+
+eval set -- "$VALID_ARGS"
+while [ : ]; do
+    case "$1" in
+        --cargo-cache-dir)
+            CARGO_CACHE_HOST="$2"
+            shift 2
+            ;;
+        --)
+            shift
+            break
+            ;;
+    esac
+done
 
 # Switch to current directory (./examples) then out to root for specific examples
 pushd $(dirname ${BASH_SOURCE[0]})
@@ -30,11 +48,22 @@ else
     TAG="latest-amd64"
 fi
 
+OPTIONAL_MOUNTS=()
+if [ ! -z $CARGO_CACHE_HOST ]; then
+    CARGO_REGISTRY="$CARGO_CACHE_HOST/registry"
+    CARGO_GIT="$CARGO_CACHE_HOST/git"
+    mkdir -p "$CARGO_REGISTRY"
+    mkdir -p "$CARGO_GIT"
+    OPTIONAL_MOUNTS+=(-v "$CARGO_REGISTRY":/usr/local/cargo/registry)
+    OPTIONAL_MOUNTS+=(-v "$CARGO_GIT":/usr/local/cargo/git)
+fi
+
 if docker ps -a --format '{{.Names}}' | grep -Eq "^$CONT_NAME\$"; then
     echo "Container exists"
 else
     docker create \
         --mount type=bind,source=$(pwd),target=/host \
+        "${OPTIONAL_MOUNTS[@]}" \
         --cap-add=SYS_PTRACE --security-opt seccomp=unconfined \
         --name="$CONT_NAME" \
         -w /host/examples/"$NAME" \
